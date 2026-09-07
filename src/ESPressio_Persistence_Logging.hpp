474abca1df7c16888594a29558e3591ed2e81141
Simon J Stuart <lakraven@gmail.com>
2026-09-07 17:22:13 +0200

    Export persistent log worker integration

--- src/ESPressio_Persistence_Logging.hpp
+++ src/ESPressio_Persistence_Logging.hpp
@@ -1,4 +1,6 @@
 #pragma once
 
-// Opt-in integration surface. ESPressio_Persistence.hpp remains free of any Logging dependency.
+// Opt-in integration surface. ESPressio_Persistence.hpp remains free of any Logging/Task dependency.
+#include <logging/ESPressio_IPersistentLogWorkTarget.hpp>
 #include <logging/ESPressio_PersistentLogSink.hpp>
+#include <logging/ESPressio_PersistentLogWorker.hpp>
